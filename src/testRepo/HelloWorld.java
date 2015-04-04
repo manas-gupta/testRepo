bb66d5cd5af10a94ddc5b3ebdf3b76cf94469133
kr0nOs <manas.niet@gmail.com>
2015-04-04 12:39:52 +0530

    Test commit!

---
+++ src/testRepo/HelloWorld.java
@@ -0,0 +1,15 @@
+package testRepo;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+public class HelloWorld 
+{
+	static final Logger logger = LogManager.getLogger(testRepo.HelloWorld.class);
+	public static void main(String[] args)
+	{
+		logger.info("Hello World!");
+
+	}
+
+}
